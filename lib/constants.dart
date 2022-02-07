c8563969d1c443b9a3b3d3fe1eb9f4bb90517e87
yasht01 <yasht.2701@gmail.com>
2022-02-07 22:01:27 +0530

    feat: ui

---
+++ lib/constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const Color kScaffoldBackgroudColor = Color(0xfffaf8ef);
+const Color kGameBoardColor = Color(0xffbbad9f);
+
+var kButtonStyle = TextButton.styleFrom(
+  backgroundColor: const Color(0xff907a65),
+  elevation: 4,
+);
+
+const TextStyle kTextStyle = TextStyle(
+  fontSize: 40,
+  fontWeight: FontWeight.w700,
+  fontFamily: 'Quicksand',
+);
---
+++ lib/screens/home_page.dart
@@ -0,0 +1,101 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:puzzle_2048/constants.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kScaffoldBackgroudColor,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: const Color(0xff907a65),
+          child: const Icon(Icons.settings),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 60),
+            child: Column(
+              children: const [
+                Text(
+                  'Score',
+                  style: kTextStyle,
+                ),
+                Text(
+                  '0',
+                  style: kTextStyle,
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: kGameBoardColor,
+            ),
+            constraints: BoxConstraints.tight(const Size.square(450)),
+            child: GridView.count(
+              physics: const NeverScrollableScrollPhysics(),
+              crossAxisCount: 4,
+              children: List.generate(
+                16,
+                (index) => Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Material(
+                    elevation: 2,
+                    borderRadius: BorderRadius.circular(20),
+                    color: const Color(0xffefe5da),
+                    child: FittedBox(
+                      fit: BoxFit.contain,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "${pow(2, index)}",
+                          style: kTextStyle,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Undo',
+                    style: kTextStyle.copyWith(color: Colors.white),
+                  ),
+                  style: kButtonStyle,
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'New Game',
+                    style: kTextStyle.copyWith(color: Colors.white),
+                  ),
+                  style: kButtonStyle,
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
