108c4c6e3790aa00db249943901d89af349dc853
yasht01 <yasht.2701@gmail.com>
2022-03-04 01:35:00 +0530

    feat: game logic + AnimatedBuilder

--- lib/components/tile.dart
+++ lib/components/tile.dart
@@ -1,7 +1,23 @@
+import 'package:flutter/animation.dart';
+
 class Tile {
   int x;
   int y;
-  int? value;
+  int value;
 
-  Tile({required this.x, required this.y, this.value});
+  late Animation<double> animatedX;
+  late Animation<double> animatedY;
+  late Animation<int> animatedValue;
+  late Animation<double> scale;
+
+  void resetAnimations() {
+    animatedX = AlwaysStoppedAnimation(x.toDouble());
+    animatedY = AlwaysStoppedAnimation(y.toDouble());
+    animatedValue = AlwaysStoppedAnimation(value);
+    scale = const AlwaysStoppedAnimation(1.0);
+  }
+
+  Tile({required this.x, required this.y, required this.value}) {
+    resetAnimations();
+  }
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:puzzle_2048/components/tile.dart';
 import '../constants.dart';
@@ -81,65 +79,79 @@ class GameBoard extends StatefulWidget {
 }
 
 class _GameBoardState extends State<GameBoard> with TickerProviderStateMixin {
-  // late final AnimationController _controller = AnimationController(
-  //   duration: const Duration(seconds: 2),
-  //   vsync: this,
-  // );
+  late AnimationController _controller;
 
-  List<List<Tile>> grid =
-      List.generate(4, (x) => List.generate(4, (y) => Tile(x: x, y: y)));
+  List<List<Tile>> grid = List.generate(
+      4, (y) => List.generate(4, (x) => Tile(x: x, y: y, value: 0)));
   Iterable<Tile> get flattenedGrid => grid.expand((element) => element);
+  Iterable<List<Tile>> get cols =>
+      List.generate(4, (x) => List.generate(4, (y) => grid[y][x]));
 
-  // void _moveRight() {
-  //   setState(() {
-  //     _boardData[0][1] = -1;
-  //     _boardData[0][3] = 1;
-  //     _tileData[1][0] = const FractionalOffset(3 / 3, 0 / 3);
-  //     _tileData[3][0] = const FractionalOffset(1 / 3, 0 / 3);
-  //   });
-  // }
+  @override
+  void initState() {
+    super.initState();
 
-  // void _moveLeft() {
-  //   setState(() {
-  //     _boardData[0][3] = -1;
-  //     _boardData[0][1] = 1;
-  //   });
-  // }
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        for (var element in flattenedGrid) {
+          element.resetAnimations();
+        }
+      }
+    });
+
+    grid[1][1].value = 4;
+    grid[1][2].value = 8;
+    grid[0][0].value = 16;
+    grid[0][1].value = 16;
+    grid[1][0].value = 32;
+
+    for (var tile in flattenedGrid) {
+      tile.resetAnimations();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    final gridSize = MediaQuery.of(context).size.height * 0.6;
+    final gridSize = MediaQuery.of(context).size.height * 0.6 - 2 * 16.0;
     final tileSize = gridSize / 4 - 2 * 4.0;
-    List<Positioned> stackItems = [];
+    List<Widget> stackItems = [];
     stackItems.addAll(
       flattenedGrid.map(
-        (e) => Positioned(
-          left: e.x * tileSize,
-          top: e.y * tileSize,
-          width: tileSize,
-          height: tileSize,
-          child: Center(
-            child: Material(
-              elevation: 2,
-              borderRadius: BorderRadius.circular(20),
-              color: const Color(0xffefe5d9),
-              child: Container(
-                width: tileSize - 2 * 4.0,
-                height: tileSize - 2 * 4.0,
-                padding: const EdgeInsets.all(8.0),
-                constraints:
-                    BoxConstraints.tight(Size.square(tileSize - 2 * 4.0)),
-                child: e.value != null 
-                    ? FittedBox(
-                        child: Text(
-                          e.value.toString(),
-                          style: kTextStyle,
+        (e) => AnimatedBuilder(
+          animation: _controller,
+          builder: (context, child) => e.animatedValue.value == 0
+              ? const SizedBox()
+              : Positioned(
+                  left: e.x * tileSize,
+                  top: e.y * tileSize,
+                  width: tileSize,
+                  height: tileSize,
+                  child: Center(
+                    child: Material(
+                      elevation: 2,
+                      borderRadius: BorderRadius.circular(20),
+                      color: const Color(0xffefe5d9),
+                      child: Container(
+                        width: tileSize - 2 * 4.0,
+                        height: tileSize - 2 * 4.0,
+                        padding: const EdgeInsets.all(8.0),
+                        constraints: BoxConstraints.tight(
+                            Size.square(tileSize - 2 * 4.0)),
+                        child: FittedBox(
+                          child: Text(
+                            e.value.toString(),
+                            style: kTextStyle,
+                          ),
                         ),
-                      )
-                    : null,
-              ),
-            ),
-          ),
+                      ),
+                    ),
+                  ),
+                ),
         ),
       ),
     );
@@ -154,13 +166,24 @@ class _GameBoardState extends State<GameBoard> with TickerProviderStateMixin {
       constraints: BoxConstraints.tight(Size.square(gridSize)),
       child: GestureDetector(
         onHorizontalDragEnd: (details) {
-          // if (details.primaryVelocity != null &&
-          //     details.primaryVelocity! > 0.0) {
-          //   _moveRight();
-          // } else if (details.primaryVelocity != null &&
-          //     details.primaryVelocity! < 0.0) {
-          //   _moveLeft();
-          // }
+          if (details.velocity.pixelsPerSecond.dx > 250 && canSwipeRight()) {
+            // Swipe Right
+            swipe(swipeRight);
+          } else if (details.velocity.pixelsPerSecond.dx < -250 &&
+              canSwipeLeft()) {
+            // Swipe Left
+            swipe(swipeLeft);
+          }
+        },
+        onVerticalDragEnd: (details) {
+          if (details.velocity.pixelsPerSecond.dy > 250 && canSwipeDown()) {
+            // Swipe Down
+            swipe(swipeDown);
+          } else if (details.velocity.pixelsPerSecond.dy < -250 &&
+              canSwipeUp()) {
+            // Swipe Up
+            swipe(swipeUp);
+          }
         },
         child: MediaQuery.removePadding(
           context: context,
@@ -171,5 +194,73 @@ class _GameBoardState extends State<GameBoard> with TickerProviderStateMixin {
         ),
       ),
     );
+  }
+
+  void swipe(void Function() swipeFn) {
+    setState(() {
+      swipeFn();
+      // add new tile
+      _controller.forward(from: 0);
+    });
+  }
+
+  bool canSwipeLeft() => grid.any(canSwipe);
+  bool canSwipeRight() => grid.map((e) => e.reversed.toList()).any(canSwipe);
+  bool canSwipeUp() => cols.any(canSwipe);
+  bool canSwipeDown() => cols.map((e) => e.reversed.toList()).any(canSwipe);
+
+  bool canSwipe(List<Tile> row) {
+    for (int i = 0; i < row.length; i++) {
+      if (row[i].value == 0) {
+        if (row.skip(i + 1).any((e) => e.value != 0)) {
+          return true;
+        }
+      } else {
+        Tile hasNonZeroVal = row.skip(i + 1).firstWhere((e) => e.value != 0,
+            orElse: () => Tile(x: 0, y: 0, value: -1));
+        if (hasNonZeroVal.value != -1 && hasNonZeroVal.value == row[i].value) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  void swipeLeft() => grid.forEach(mergeTiles);
+  void swipeRight() => grid.map((e) => e.reversed.toList()).forEach(mergeTiles);
+  void swipeUp() => cols.forEach(mergeTiles);
+  void swipeDown() => cols.map((e) => e.reversed.toList()).forEach(mergeTiles);
+
+  void mergeTiles(List<Tile> tiles) {
+    // Find the first non-zero tile
+    // Find the second non-zero tile
+    // merge tiles *if possible*
+    // Repeat from step 1 until the end of the Iterable<Tile>
+
+    for (int i = 0; i < tiles.length; i++) {
+      List<Tile> toCheck =
+          tiles.skip(i).skipWhile((tile) => tile.value == 0).toList();
+      if (toCheck.isNotEmpty) {
+        Tile firstNonZeroTile = toCheck.first;
+        Tile secondNonZeroTile = toCheck.skip(i + 1).firstWhere(
+            (tile) => tile.value != 0,
+            orElse: () => Tile(x: 0, y: 0, value: -1));
+
+        if (secondNonZeroTile.value != -1 &&
+            secondNonZeroTile.value != firstNonZeroTile.value) {
+          secondNonZeroTile = Tile(x: 0, y: 0, value: -1);
+        }
+
+        if (tiles[i] != firstNonZeroTile || secondNonZeroTile.value != -1) {
+          int newValue = firstNonZeroTile.value;
+          if (secondNonZeroTile.value != -1) {
+            newValue += secondNonZeroTile.value;
+            secondNonZeroTile.value = 0;
+          }
+          firstNonZeroTile.value = 0;
+          tiles[i].value = newValue;
+        }
+      }
+    }
   }
 }
